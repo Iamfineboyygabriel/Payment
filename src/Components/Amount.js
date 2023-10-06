59b1353bbc77e3ee2548fed609d97d5bf37389c9
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-10-06 15:52:12 +0100

    a payment platform practice

---
+++ src/Components/Amount.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import usa from "../Assets/images/usa.png"
+import "../Styles/Amount.css"
+
+const Amount = () => {
+  return (
+    <div className='container__house'>
+      <div className='one-time'>
+       <h3>One-time Payment</h3>
+       <p>Send money internationally</p>
+      </div>
+
+
+
+
+    
+  
+
+
+
+    </div>
+  )
+}
+
+export default Amount
---
+++ src/Payment/Payment.js
@@ -0,0 +1,18 @@
+// Payment.js
+import React from 'react';
+import Navbar from "../Components/Navbar";
+
+function Payment() {
+  // Your Payment component code here
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Payment;
+
+
+
+
